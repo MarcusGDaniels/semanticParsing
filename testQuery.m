96d1d77b5a06a04342f30b6ddbf155e71dc3fdc3
Marcus G Daniels <marcus@snoutfarm.com>
2024-01-04 10:45:09 -0800

    sample of use of sentences

---
+++ testQuery.m
@@ -0,0 +1,14 @@
+:- module testQuery.
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module mrs.
+:- import_module list.
+:- import_module sentences.
+:- import_module sentence_accessors.
+
+main(!IO) :-
+  io.print(det_head(sentences.sentences),!IO),
+  io.nl(!IO). 
