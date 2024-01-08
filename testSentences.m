b0c8d4bf7c784708c57e1aba8d6cee1291b69c0f
Marcus G Daniels <marcus@snoutfarm.com>
2024-01-08 12:31:48 -0800

    Illustrate how to query across sentences and collect terms.

--- testSentences.m
+++ testSentences.m
@@ -7,11 +7,61 @@
 :- import_module mrs.
 :- import_module calls.
 :- import_module list.
+:- import_module multi_map.
+:- import_module sentences.
+:- import_module sentence_accessors.
 
 :- import_module solutions.
 
+:- func collectRelations(mrs_rel_handle) = list(preds).
+collectRelations(RelHandle) = L :-
+  list.foldl(pred(psoa_post(_,_,MM)::in,LIn::in,LOut::out) is det :- 
+    ((if multi_map.contains(MM,RelHandle) then
+        LL = multi_map.lookup(MM,RelHandle)
+      else
+        LL = []),
+     Preds = list.map(func({_,_,_,Pred}) = Ret :- Ret = Pred,LL),
+     LOut = LIn ++ Preds),
+    sentences.sentences, [], L).
+
+:- pred unpackRelation(mrs_rel_handle,preds,io,io).
+:- mode unpackRelation(in,in,di,uo) is det.
+unpackRelation(RelHandle,Pred,IoIn,IoOut) :-
+  if pred_therein_p_dir(_) = Pred then
+    io.print("therein_p_dir(",IoIn,Io0),
+    solutions(pred(E1::out) is nondet :- therein_p_dir(RelHandle,E1,E2), E1L),
+    solutions(pred(E2::out) is nondet :- therein_p_dir(RelHandle,E1,E2), E2L),
+    io.print({E1L,E2L},Io0,Io1),
+    io.print(")",Io1,Io2),
+    io.nl(Io2,IoOut)
+  else if pred_live_v_1(_) = Pred then
+    io.print("live_v_1(",IoIn,Io0),
+    solutions(pred(E::out) is nondet :- live_v_1(RelHandle,E,Inst), EL),
+    solutions(pred(Inst::out) is nondet :- live_v_1(RelHandle,E,Inst), IL),
+    io.print({EL,IL},Io0,Io1),
+    io.print(")",Io1,Io2),
+    io.nl(Io2,IoOut)
+  else if pred_people_n_of(_) = Pred then
+    io.print("people_n_of",IoIn,Io0),
+    solutions(pred(Inst::out) is nondet :- people_n_of(RelHandle,Inst,Indiv), InstL),
+    solutions(pred(Indiv::out) is nondet :- people_n_of(RelHandle,Inst,Indiv), IndivL),
+    io.print({InstL,IndivL},Io0,Io1),
+    io.print(")",Io1,Io2),
+    io.nl(Io2,IoOut)
+  else if pred_only_a_1(_) = Pred then
+    io.print("only_a_1",IoIn,Io0),
+    solutions(pred(E::out) is nondet :- only_a_1(RelHandle,E,Inst), EL),
+    solutions(pred(Inst::out) is nondet :- only_a_1(RelHandle,E,Inst), IL),
+    io.print({EL,IL},Io0,Io1),
+    io.print(")",Io1,Io2),
+    io.nl(Io2,IoOut)
+  else
+    io.print("unknown predicate",IoIn,IoOut).
+		   
+
 main(!IO) :-
-  solutions(pred(E::out) is nondet :- kill_v_1(RelHandle,E,A,B),L2),
-  io.print(to_string(det_head(L2)),!IO),
-  io.print(to_string(det_head(det_tail(L2))),!IO),
-  io.nl(!IO). 
+  solutions(pred(RelHandle::out) is nondet :- only_a_1(RelHandle,E,A), Handles),
+  list.foldl(pred(RelHandle::in,IoIn::di,IoOut::uo) is det :- 
+    (Preds = collectRelations(RelHandle),
+     list.foldl(unpackRelation(RelHandle), Preds, IoIn, IoOut)),
+    Handles, !IO).
